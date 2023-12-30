6ad2db92c98d06e9829c59c7e059edd9a5f8a906
Abhijith <abhijithtp2003@gmail.com>
2023-12-30 23:59:53 +0530

    register page made

--- lib/register.dart
+++ lib/register.dart
@@ -8,8 +8,53 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+
+  final TextEditingController _fullNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void register() {
+    // Implement your registration logic here
+    print("Registering...");
+    print("Full Name: ${_fullNameController.text}");
+    print("Email: ${_emailController.text}");
+    print("Password: ${_passwordController.text}");
+  }
   @override
   Widget build(BuildContext context) {
-    return Text("register");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Register"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: _fullNameController,
+              decoration: InputDecoration(labelText: "Full Name"),
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: "Email"),
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(labelText: "Password"),
+            ),
+            SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: register,
+              child: Text("Register"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

d6b8fcf698c8932bd5ed651d23e2e7f25ddb359d
Abhijith <abhijithtp2003@gmail.com>
2024-02-06 00:18:41 +0530

    login  formfield changed

--- lib/login.dart
+++ lib/login.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:link/dashboard.dart';
+import 'package:link/main.dart';
 import 'package:link/register.dart';
 
 class Login extends StatefulWidget {
-  const Login({super.key});
+  const Login({Key? key});
 
   @override
   State<Login> createState() => _LoginState();
@@ -15,16 +17,10 @@ class _LoginState extends State<Login> {
   bool _obs_text = true;
 
   void redirectToRegister() {
-    // Using MaterialPageRoute
     Navigator.push(
       context,
-      MaterialPageRoute(
-          builder: (context) =>
-              RegisterScreen()), // Replace RegisterScreen with the actual name of your registration screen
+      MaterialPageRoute(builder: (context) => RegisterScreen()),
     );
-
-    // If you have named routes, you can use Navigator.pushNamed
-    // Navigator.pushNamed(context, '/register');
   }
 
   void login() {
@@ -33,7 +29,9 @@ class _LoginState extends State<Login> {
 
     if (_name.text == _password.text) {
       Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => Dashboard()));
+        context,
+        MaterialPageRoute(builder: (context) => Dashboard()),
+      );
     }
   }
 
@@ -41,9 +39,7 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading:
-            false, // Add this line to disable the leading icon
-
+        automaticallyImplyLeading: false,
         title: Padding(
           padding: const EdgeInsets.all(35.0),
           child: Column(
@@ -52,14 +48,15 @@ class _LoginState extends State<Login> {
               Text(
                 "Welcome To Link",
                 style: TextStyle(
-                    color: const Color.fromRGBO(21, 101, 192, 1),
-                    fontWeight: FontWeight.w800,
-                    fontSize: 40),
+                  color: const Color.fromRGBO(21, 101, 192, 1),
+                  fontWeight: FontWeight.w800,
+                  fontSize: 40,
+                ),
               ),
               Text(
                 "the complete legal app",
                 style: TextStyle(color: Colors.black54, fontSize: 17),
-              )
+              ),
             ],
           ),
         ),
@@ -68,109 +65,99 @@ class _LoginState extends State<Login> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(35.0),
-          child: Container(
-            // color: Colors.deepOrange,
-            child: Column(
-              children: [
-                SingleChildScrollView(
-                  child: Flexible(
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          TextFormField(
-                            decoration: InputDecoration(hintText: "Username"),
-                            controller: _name,
-                          ),
-                          SizedBox(
-                            height: 30,
-                          ),
-                          TextFormField(
-                            obscureText: _obs_text,
-                            decoration: InputDecoration(
-                                hintText: "Password",
-                                suffix: InkWell(
-                                  child: Icon(Icons.remove_red_eye_outlined),
-                                  onTap: () {
-                                    _obs_text = !_obs_text;
-                                    setState(() {});
-                                  },
-                                )),
-                            controller: _password,
-                          ),
-                          SizedBox(
-                            height: 30,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              GestureDetector(
-                                  onTap: () {
-                                    print("Forget password");
-                                  },
-                                  child: Text("Forgoten password",
-                                      style: TextStyle(
-                                          color: Colors.blue,
-                                          fontWeight: FontWeight.w800))),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Row(
-                            children: [
-                              Expanded(
-                                child: ElevatedButton(
-                                  onPressed: () {
-                                    login();
-                                  },
-                                  child: Text(
-                                    "Login",
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                  style: ButtonStyle(
-                                    backgroundColor: MaterialStateProperty.all<
-                                            Color>(
-                                        const Color.fromRGBO(21, 101, 192, 1)),
-                                    shape: MaterialStateProperty.all<
-                                        RoundedRectangleBorder>(
-                                      RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(5.0),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              SizedBox(
-                                height: 50,
-                              ),
-                              Text(
-                                "dont have an account ?  ",
-                                style: TextStyle(fontSize: 17),
-                              ),
-                              GestureDetector(
-                                onTap: redirectToRegister,
-                                child: Text(
-                                  "Signup now!",
-                                  style: TextStyle(
-                                    color: Colors.blueAccent,
-                                    fontSize: 17,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          )
-                        ]),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+             
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Username",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: appcolor.primary))
+                ),
+                controller: _name,
+              ),
+              SizedBox(height: 30),
+              TextFormField(
+                obscureText: _obs_text,
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  suffixIcon: InkWell(
+                    child: Icon(Icons.remove_red_eye_outlined),
+                    onTap: () {
+                      setState(() {
+                        _obs_text = !_obs_text;
+                      });
+                    },
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: appcolor.primary))
+
+                ),
+                controller: _password,
+              ),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      print("Forget password");
+                    },
+                    child: Text(
+                      "Forgotten password",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: login,
+                child: Text(
+                  "Login",
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                    const Color.fromRGBO(21, 101, 192, 1),
+                  ),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
                   ),
                 ),
-              ],
-            ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account? ",
+                    style: TextStyle(fontSize: 17),
+                  ),
+                  GestureDetector(
+                    onTap: redirectToRegister,
+                    child: Text(
+                      "Signup now!",
+                      style: TextStyle(
+                        color: Colors.blueAccent,
+                        fontSize: 17,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
           ),
         ),
       ),
